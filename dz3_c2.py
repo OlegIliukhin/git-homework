966dce2faaf7f9df7a4d3a2baebccb077b84ebcd
OlegIliukhin <6329839@gmail.com>
2026-08-26 13:15:05 +0300

    Improve homework 3.1 functions

--- dz3_c2.py
+++ dz3_c2.py
@@ -1,5 +1,5 @@
 # ДЗ 3.1. Робота з типами даних
-
+#переписал функції check_even, get_even_numbers, common_elements
 
 # ============================================================
 # 1. РЯДКИ (STRINGS)
@@ -53,6 +53,15 @@ def common_elements(list1, list2):
 
     return result
 
+#### переписал функції common_elements
+
+def common_elements_new(list1: list[int], list2: list[int]) -> list[int]:
+    """Повертае спільні елементи двох списків."""
+    return [item for item in list1 if item in list2]
+
+a_new = [7, 8, 9, 5]
+b_new = [2, 7, 3, 8]
+
 
 # ============================================================
 # 4. СЛОВНИКИ (DICTIONARIES)
@@ -94,6 +103,12 @@ def check_even(number):
     else:
         print("Непарне")
 
+#### переписал функції check_even
+
+def check_even_new(number: int) -> str:
+    """Повертає 'Парне' або 'Непарне' залежно від числа."""
+    return "Парне" if number % 2 == 0 else "Непарне"
+
 
 # 6.2. Список тільки парних чисел
 def get_even_numbers(numbers):
@@ -104,6 +119,13 @@ def get_even_numbers(numbers):
             result.append(number)
 
     return result
+
+####переписал функції  get_even_numbers
+
+def get_even_numbers_new(numbers: list[int]) -> list[int]:
+    """Повертає список тільки з парних чисел."""
+    return [number for number in numbers if number % 2 == 0]
+
 
 
 # ============================================================
@@ -126,22 +148,25 @@ print("2.2:", add_numbers(10, 20))
 print("2.3:", divide_int(17, 5))
 
 print("3.1:", average([10, 20, 30]))
+
 print("3.2:", common_elements([1, 2, 3, 4], [3, 4, 5, 6]))
+print("3.2 (исправленный варіант):", common_elements_new(a_new, b_new))
 
 print("4.1:")
 print_keys({"name": "Oleg", "age": 49, "city": "Dnipro"})
 
 print("4.2:", merge_dicts(
     {"name": "Oleg", "age": 49},
-    {"city": "Dnipro", "job": "Python"}
-))
+    {"city": "Dnipro", "job": "Python"}))
 
 print("5.1:", union_sets({1, 2, 3}, {3, 4, 5}))
 print("5.2:", is_subset({1, 2}, {1, 2, 3}))
 
-print("6.1:")
+print("6.1:") 
 check_even(8)
+print("6.1 (переписанный варіант):", check_even_new(8))
 
 print("6.2:", get_even_numbers([1, 2, 3, 4, 5, 6]))
+print("6.2 (переписанный вариант):", get_even_numbers_new([1, 2, 3, 4, 5, 6]))
 
 print("7.1:", is_even(7))
